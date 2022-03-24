c8609c7fb466b25a10431fd1a11ecd0a7106c614
Yossawadee Hoymala <h.yossawadee@gmail.com>
2022-03-24 18:59:48 +0700

    mvc3 movie

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Genre must be specified")]
+        public string Genre { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Price must be between $1 and $100")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal Price { get; set; }
+
+        [StringLength(5)]
+        public string Rating { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
